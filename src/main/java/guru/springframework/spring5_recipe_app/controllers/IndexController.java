9298e44a4702a765e5a522fb91f958f2631fa299
Devika <devika@Devikas-MacBook-Air.local>
2024-10-06 19:44:55 -0700

    First Commit

---
+++ src/main/java/guru/springframework/spring5_recipe_app/controllers/IndexController.java
@@ -0,0 +1,14 @@
+package guru.springframework.spring5_recipe_app.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping({"","/","index"})
+    public String getIndexPage(){
+
+        return "index";
+    }
+}
