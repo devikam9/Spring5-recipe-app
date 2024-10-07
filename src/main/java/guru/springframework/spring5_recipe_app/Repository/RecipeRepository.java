0c4c7dd2a59812ad39dbe5ff84ee5bc62af4dd24
Devika <devika@Devikas-MacBook-Air.local>
2024-10-07 10:52:39 -0700

    Added Repositories for recipes, ingredients,unitofmeasure

--- src/main/java/guru/springframework/spring5_recipe_app/Repository/RecipeRepository.java
+++ src/main/java/guru/springframework/spring5_recipe_app/Repository/RecipeRepository.java
@@ -1,4 +1,7 @@
 package guru.springframework.spring5_recipe_app.Repository;
 
-public class RecipeRepository {
+import guru.springframework.spring5_recipe_app.domain.Recipe;
+import org.springframework.data.repository.CrudRepository;
+
+public interface RecipeRepository extends CrudRepository<Recipe, Long> {
 }
---
+++ src/main/java/guru/springframework/spring5_recipe_app/Repository/UnitofMeasureRepository.java
@@ -0,0 +1,7 @@
+package guru.springframework.spring5_recipe_app.Repository;
+
+import guru.springframework.spring5_recipe_app.domain.UnitofMeasure;
+import org.springframework.data.repository.CrudRepository;
+
+public interface UnitofMeasureRepository extends CrudRepository<UnitofMeasure, Long> {
+}
